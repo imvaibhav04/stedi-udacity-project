5900e1557cc092692a3e4fa21e74ce458c855edb
Vaibhav Raj <114829392+imvaibhav04@users.noreply.github.com>
2026-01-21 15:20:07 +0530

    Add initial AWS Glue job script for data processing

---
+++ step_trainer_landing.py
@@ -0,0 +1,18 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1768988525845 = glueContext.create_dynamic_frame.from_catalog(database="stedi-udacitydb-vr", table_name="step_trainerdata", transformation_ctx="AWSGlueDataCatalog_node1768988525845")
+
+job.commit()
